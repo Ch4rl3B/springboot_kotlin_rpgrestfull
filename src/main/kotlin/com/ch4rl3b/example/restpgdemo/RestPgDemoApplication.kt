888f6c1d41091839ea61b91630048bfc20561aae
ernesto.caballero <ernesto.caballero@datys.cu>
2021-06-12 21:53:49 -0400

    project init

---
+++ src/main/kotlin/com/ch4rl3b/example/restpgdemo/RestPgDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ch4rl3b.example.restpgdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestPgDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestPgDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ch4rl3b/example/restpgdemo/RestPgDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ch4rl3b.example.restpgdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestPgDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
